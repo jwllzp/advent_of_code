9622916f2c91a5eb692a143b52585472f603a352
jwllzp <35391305+jwllzp@users.noreply.github.com>
2025-12-14 03:05:27 -0600

    add node structure

--- src/days/d07.rs
+++ src/days/d07.rs
@@ -1,4 +1,7 @@
-use std::collections::HashSet;
+use std::collections::{
+    HashMap,
+    HashSet,
+};
 use std::fmt;
 use std::fs;
 
@@ -9,9 +12,29 @@ pub struct TachyonManifold {
 }
 
 #[derive(Debug)]
+pub struct Graph {
+    nodes: HashMap<usize, Node>,
+    initial_node_id: usize,
+    terminal_node_ids: Vec<usize>,
+}
+
+#[derive(Clone, Debug)]
 enum Obstacle {
     Space,
     Splitter,
+}
+
+#[derive(Clone, Debug)]
+enum Node {
+    Initial(usize),
+    Splitter(SplitterNode),
+    End(usize),
+}
+
+#[derive(Clone, Debug)]
+struct SplitterNode {
+    id: usize,
+    children: Vec<usize>,
 }
 
 #[derive(Debug)]
@@ -80,10 +103,6 @@ impl TachyonManifold {
         splits
     }
 
-    pub fn part_2(&self) -> usize {
-        todo!();
-    }
-
     fn get_beam_postions(&self) -> HashSet<usize> {
         self.beams.iter().map(|b| b.position).collect()
     }
@@ -100,6 +119,97 @@ impl fmt::Display for Obstacle {
         match self {
             Obstacle::Space => write!(f, "."),
             Obstacle::Splitter => write!(f, "^"),
+        }
+    }
+}
+
+impl Graph {
+    pub fn new(path: &str) -> Self {
+        let raw = fs::read_to_string(path).unwrap();
+        let mut lines = raw.trim().lines();
+        let mut node_id = 0;
+
+        let mut diagram: Vec<Vec<Option<Node>>> = lines
+            .map(|l| {
+                l.chars()
+                    .map(|c| {
+                        let node = match c {
+                            'S' => Some(Node::Initial(node_id)),
+                            '^' => Some(Node::Splitter(SplitterNode {
+                                id: node_id,
+                                children: Vec::new(),
+                            })),
+                            _ => None,
+                        };
+                        node_id += 1;
+                        node
+                    })
+                    .collect()
+            })
+            .collect();
+
+        let mut terminal_nodes = Vec::new();
+        for _ in 0..diagram.len() {
+            terminal_nodes.push(Some(Node::End(node_id)));
+            node_id += 1;
+        }
+        diagram.push(terminal_nodes);
+
+        let mut nodes: HashMap<usize, Node> = HashMap::new();
+        let initial_node_id: usize = 0;
+        let mut terminal_node_ids: Vec<usize> = Vec::new();
+        for (row_idx, row) in diagram.iter().enumerate() {
+            for (col_idx, col) in row.iter().enumerate() {
+                if let Some(node) = col {
+                    match node {
+                        Node::Initial(id) => {
+                            nodes.insert(*id, node.clone());
+                        }
+                        Node::Splitter(splitter_node) => {
+                            let mut splitter_node = splitter_node.clone();
+                            splitter_node.children = Self::get_child_ids(
+                                &row_idx, &col_idx, &diagram,
+                            );
+                            nodes.insert(
+                                splitter_node.id,
+                                Node::Splitter(splitter_node),
+                            );
+                        }
+                        Node::End(id) => {
+                            nodes.insert(*id, node.clone());
+                            terminal_node_ids.push(*id);
+                        }
+                    }
+                }
+            }
+        }
+
+        Graph {
+            nodes,
+            initial_node_id,
+            terminal_node_ids,
+        }
+    }
+
+    fn get_child_ids(
+        row_idx: &usize,
+        col_idx: &usize,
+        diagram: &Vec<Vec<Option<Node>>>,
+    ) -> Vec<usize> {
+        todo!()
+    }
+
+    pub fn propagate(&self) -> usize {
+        todo!();
+    }
+}
+
+impl Node {
+    fn new(id: usize, child_ids: Vec<usize>, node_type: NodeType) -> Self {
+        Node {
+            id,
+            child_ids,
+            node_type,
         }
     }
 }
@@ -122,8 +232,8 @@ mod d07 {
 
     #[test]
     fn test_part2_example() {
-        let manifold = TachyonManifold::new("src/days/inputs/07/example.txt");
-        assert_eq!(40, manifold.part_2());
+        let manifold = Graph::new("src/days/inputs/07/example.txt");
+        assert_eq!(40, manifold.propagate());
     }
 
     #[test]
