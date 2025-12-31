02d0236a68ec5c0d3393388f3f4c390edb1ce3ab
jwllzp <35391305+jwllzp@users.noreply.github.com>
2025-12-31 03:11:47 -0600

    eod

--- src/days/d09.rs
+++ src/days/d09.rs
@@ -1,5 +1,18 @@
-#[derive(Debug)]
+use std::cmp::Ordering;
+use std::collections::HashSet;
+
+#[derive(Debug, Eq, PartialEq, Hash)]
 struct Point(u64, u64);
+
+impl Point {
+    fn eq(&self, point: &Point) -> bool {
+        (self.0 == point.0) && (self.1 == point.1)
+    }
+
+    fn is_aligned(&self, point: &Point) -> bool {
+        self.0 == point.0 || self.1 == point.1
+    }
+}
 
 #[derive(Debug)]
 struct Rectangle<'a> {
@@ -26,6 +39,100 @@ impl<'a> Rectangle<'a> {
             height,
             area,
         }
+    }
+
+    fn get_corners(&self) -> Vec<&'a Point> {
+        let p1 = self.coords.0;
+        let p2 = self.coords.1;
+
+        let (p3, p4) = match (p1.0.cmp(&p2.0), p1.1.cmp(&p2.1)) {
+            (Ordering::Less, Ordering::Less) => todo!(),
+            (Ordering::Less, Ordering::Equal) => todo!(),
+            (Ordering::Less, Ordering::Greater) => todo!(),
+            (Ordering::Equal, Ordering::Less) => todo!(),
+            (Ordering::Equal, Ordering::Equal) => todo!(),
+            (Ordering::Equal, Ordering::Greater) => todo!(),
+            (Ordering::Greater, Ordering::Less) => todo!(),
+            (Ordering::Greater, Ordering::Equal) => todo!(),
+            (Ordering::Greater, Ordering::Greater) => todo!(),
+        };
+
+        vec![p1, p2, p3, p4]
+    }
+}
+
+#[derive(Debug)]
+struct Region {
+    segments: Vec<Segment>,
+}
+
+#[derive(Debug)]
+struct Segment {
+    orientation: Orientation,
+    start: u64,
+    end: u64,
+}
+
+#[derive(Debug)]
+enum Orientation {
+    Horizontal(u64),
+    Vertical(u64),
+}
+
+impl Region {
+    fn new(points: &Vec<Point>) -> Self {
+        let mut connected_pairs: HashSet<(&Point, &Point)> = HashSet::new();
+        let mut segments: Vec<Segment> = Vec::new();
+
+        for p1 in points {
+            for p2 in points {
+                if p1.eq(p2) || connected_pairs.contains(&(p1, p2)) {
+                    continue;
+                }
+
+                if p1.is_aligned(p2) {
+                    segments.push(Segment::new(p1, p2).unwrap());
+                }
+
+                connected_pairs.insert((p2, p1));
+            }
+        }
+
+        Region { segments }
+    }
+
+    fn contains_rectangle(&self, rectancle: Rectangle) {
+        todo!()
+    }
+}
+
+#[derive(Debug)]
+enum SegmentError {
+    NonColinearPoints,
+}
+
+impl Segment {
+    fn new(p1: &Point, p2: &Point) -> Result<Self, SegmentError> {
+        if (p1.0 != p2.0) && (p1.1 != p2.1) {
+            return Err(SegmentError::NonColinearPoints);
+        }
+
+        let orientation = if p1.0 == p2.0 {
+            Orientation::Vertical(p1.0)
+        } else {
+            Orientation::Horizontal(p1.1)
+        };
+
+        let (start, end) = match orientation {
+            Orientation::Horizontal(_) => (p1.0.min(p2.0), p1.0.max(p2.0)),
+            Orientation::Vertical(_) => (p1.1.min(p2.1), p1.1.max(p2.1)),
+        };
+
+        Ok(Segment {
+            orientation,
+            start,
+            end,
+        })
     }
 }
 
@@ -58,6 +165,21 @@ pub fn part_1(path: &str) -> u64 {
     max_area
 }
 
+pub fn part_2(path: &str) -> u64 {
+    let points = load_points(path);
+    let region = Region::new(&points);
+    dbg!(region);
+
+    let mut max_area = 0;
+    for (idx, p1) in points[..points.len()].iter().enumerate() {
+        for p2 in &points[(idx + 1)..] {
+            let rectangle = Rectangle::new(p1, p2);
+            max_area = max_area.max(rectangle.area);
+        }
+    }
+    max_area
+}
+
 #[cfg(test)]
 mod d09 {
     use super::*;
@@ -76,7 +198,8 @@ mod d09 {
 
     #[test]
     fn test_part_2_example() {
-        todo!()
+        let output = part_2("src/days/inputs/09/example.txt");
+        assert_eq!(24, output);
     }
 
     #[test]
